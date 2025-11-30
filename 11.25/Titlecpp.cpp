89be37a8c7e75f6d334972feaa65492219e941c0
エネゴリ もどき <gigamaxstaf@gmail.com>
2025-11-30 13:05:56 +0900

    BGM追加

--- 11.25/Titlecpp.cpp
+++ 11.25/Titlecpp.cpp
@@ -2,8 +2,8 @@
 #include	<time.h>
 #include	"MainInclude.h"
 
-static void TitleScene(int time,int& scene);
-static void ResultScene(Massage* massage);
+static void TitleScene(int time,int& scene,const int bgm[]);
+static void ResultScene(Massage* massage,const int bgm[]);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
 	SetWindowText("ÉäÉYÉÄÉQÅ[ÉÄ");
@@ -15,6 +15,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	time_t startTime = time(NULL);
 	int scene = TITLE;
+
+	static const int bgm[] = {
+		LoadSoundMem("sound/8bit_Game_Menu.mp3"),
+		LoadSoundMem("sound/Red_Glow.mp3")
+	};
+
 	Massage* score = nullptr;
 	while (1) {
 		ClearDrawScreen();
@@ -26,11 +32,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 		switch (scene) {
 		case TITLE:
-			TitleScene(nowTime, scene); break;
+			TitleScene(nowTime, scene,bgm); break;
 		case GAME:
 			score = Game(scene,time(NULL)); break;
 		case RESULT:
-			ResultScene(score); break;
+			ResultScene(score,bgm); break;
 		}
 
 		ScreenFlip();
@@ -44,16 +50,35 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 }
 
-static void TitleScene(int time,int& scene) {
+static void TitleScene(int time,int& scene,const int bgm[]) {
+	static bool play = true;
+
+	if (play) {
+		ChangeVolumeSoundMem(256, bgm[0]);
+		PlaySoundMem(bgm[0], DX_PLAYTYPE_LOOP);
+		play = false;
+	}
+
 	SetFontSize(100);
 	DrawString(WIDTH / 2 - 300, 100, "Rythem Game", WHITE);
 
 	SetFontSize(50);
 	if (time % 2 != 0)DrawString(WIDTH / 2 - 140, HEIGHT - 110, "Push SPACE", WHITE);
-	if (CheckHitKey(KEY_INPUT_SPACE) == 1) scene = GAME;
+	if (CheckHitKey(KEY_INPUT_SPACE) == 1) {
+		StopSoundMem(bgm[0]);
+		scene = GAME;
+	}
 }
 
-static void ResultScene(Massage* massage) {
+static void ResultScene(Massage* massage,const int bgm[]) {
+	static bool play = true;
+
+	if (play) {
+		ChangeVolumeSoundMem(256, bgm[1]);
+		PlaySoundMem(bgm[1], DX_PLAYTYPE_LOOP);
+		play = false;
+	}
+
 	SetFontSize(100);
 	DrawString(WIDTH / 2 - 110, 100, "åãâ ", RED);
 	SetFontSize(50);
